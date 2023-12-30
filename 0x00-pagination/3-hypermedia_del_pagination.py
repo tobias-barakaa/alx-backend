2cf56fca4c0c1ac659711234b30a15e832cce0f7
tobias-barakaa <ngairato@gmail.com>
2023-12-30 04:43:04 -0800

    3-hypermedia_del_pagination.py

---
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+"""
+index_range - functions.
+0x00. Python - Variable Annotations
+"""
+import csv
+from typing import List
+
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Return a tuple of size two containing a start index and an end index
+        corresponding to the range of indexes to return in a list for those
+        particular pagination parameters.
+    """
+    if page <= 0 or page_size <= 0:
+        raise ValueError("Both page and page_size must be positive integers.")
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATAFILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        """
+        Initialize instance.
+        """
+        self.__dataset = None
+
+    def dataset(self) -> List[str]:
+        """
+        Dataset getter.
+        """
+        return self.__dataset
+
+    def get_dataset(self) -> List[str]:
+        """
+        Get dataset.
+        """
+        if self.__dataset is None:
+            with open(self.DATAFILE, "r") as f:
+                reader = csv.reader(f)
+                dataset = []
+                for row in reader:
+                    dataset.append(row)
+            self.__dataset = dataset[1:]
+        return self.__dataset
+
+    @staticmethod
+    def get_page(page: int = 1, page_size: int = 10) -> List[str]:
+        """
+        Get page.
+        """
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        server = Server()
+        start, end = index_range(page, page_size)
+        return server.get_dataset()[start:end]
+    
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        Get hyper.
+        """
+        total_pages = len(self.get_dataset()) / page_size
+        if len(self.get_dataset()) % page_size != 0:
+            total_pages += 1
+        return {
+            "page_size": page_size if page < total_pages else 0,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": page + 1 if page + 1 < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
+    
+    def get_hyper_index(self, index: int = 1, page_size: int = 10) -> dict:
+        """
+        Get hyper index.
+        """
+        assert isinstance(index, int) and index > 0
+        assert isinstance(page_size, int) and page_size > 0
+        total_pages = len(self.get_dataset()) / page_size
+        if len(self.get_dataset()) % page_size != 0:
+            total_pages += 1
+        page = index / page_size
+        if index % page_size != 0:
+            page += 1
+        page = int(page)
+        return {
+            "index": index,
+            "page_size": page_size if page < total_pages else 0,
+            "page": page,
+            "data": self.get_page(page, page_size),
+            "next_page": page + 1 if page + 1 < total_pages else None,
+            "prev_page": page - 1 if page > 1 else None,
+            "total_pages": total_pages
+        }
