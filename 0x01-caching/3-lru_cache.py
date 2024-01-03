8f5022b285f66073d63fccd0e2df1ba7518990e2
root <ngairato@gmail.com>
2024-01-03 22:53:11 +0000

    3-lru_cache.py

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""
+class that inherits from BaseCaching and is a caching system
+"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    class LRUCache that inherits from BaseCaching and is a caching system
+    """
+    def __init__(self):
+        """
+        constructor
+        """
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """
+        Must assign to the dictionary self.cache_data the item value
+        for the key key.
+        """
+        if key and item:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                if key in self.cache_data:
+                    self.queue.remove(key)
+                else:
+                    del self.cache_data[self.queue[0]]
+                    print("DISCARD:", self.queue[0])
+                    self.queue.pop(0)
+            self.cache_data[key] = item
+            self.queue.append(key)
+
+    def get(self, key):
+        """
+        Must return the value in self.cache_data linked to key.
+        """
+        if key in self.cache_data:
+            self.queue.remove(key)
+            self.queue.append(key)
+            return self.cache_data[key]
+        return None
