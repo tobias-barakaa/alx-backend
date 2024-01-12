a29047b69ea008673a8670559e1abee544622133
tobias-barakaa <ngairato@gmail.com>
2024-01-12 07:39:09 -0800

    2-app.py

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -18,13 +18,13 @@ app.config.from_object(Config)
 
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """get locale"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
-def index():
+def index() -> str:
     """index"""
     return render_template('2-index.html')
 
