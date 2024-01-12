36a53fec3080f32507f05a3f887d402d05376e85
tobias-barakaa <ngairato@gmail.com>
2024-01-12 15:03:14 -0800

    more changes

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -22,18 +22,17 @@ app.config.from_object(Config)
 
 @babel.localeselector
 def get_locale() -> str:
-    """get locale"""
-      # Check if the 'locale' parameter is present in the request's query string
-    requested_locale = request.args.get('locale')
-    
-    if requested_locale and requested_locale in app.config['LANGUAGES']:
-        return requested_locale
-    
-    # If the parameter is not present or not a supported locale, resort to the default behavior
-    return request.accept_languages.best_match(app.config['LANGUAGES'])
+    """
+    get locale from request or use default behavior.
+    """
+    locale = request.args.get('locale')
+    if locale and locale in app.config['LANGUAGES']:
+        return locale
+    else:
+        return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/')
 def index() -> str:
     """index"""
-    return render_template('4-index.html')
+    return render_template('3-index.html')
