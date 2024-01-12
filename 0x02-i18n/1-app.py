0ac28da67aa4dd0cb7298b838674f5c8b320e363
tobias-barakaa <ngairato@gmail.com>
2024-01-12 05:38:28 -0800

    more changes

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -5,19 +5,21 @@ date configure place
 from flask import Flask, render_template
 from flask_babel import Babel
 
-app = Flask(__name__)
-babel = Babel(app)
 
-
-class Config:
+class Config(object):
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
 
 
+app = Flask(__name__)
+babel = Babel(app)
 app.config.from_object(Config)
 
 
 @app.route('/')
 def index():
+    """
+    function index: to render html
+    """
     return render_template('1-index.html')
