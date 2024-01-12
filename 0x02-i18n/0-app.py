372fbde1eb1758c65e3208f74b7ca6b41f7cb1f1
tobias-barakaa <ngairato@gmail.com>
2024-01-12 02:04:26 -0800

    trying to connect flask to html

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    function index
+    """
     return render_template('0-index.html')
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
